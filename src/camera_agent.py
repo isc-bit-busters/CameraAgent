269fe5b1c3200ddc8fc615b757bb331369e77b29
Dimitri Imfeld <dimfeld@protonmail.ch>
2025-04-11 11:10:19 +0200

    Send image behaviour

--- src/camera_agent.py
+++ src/camera_agent.py
@@ -5,20 +5,28 @@ import aiofiles
 from spade import agent, behaviour
 from spade.message import Message
 
+SEND_PHOTO_INTERVAL = 5 # seconds
 
 class CameraAgent(agent.Agent):
+
     def __init__(self, jid, password):
         super().__init__(jid, password)
+        self.last_sent_time = None
+        self.registered_agents = []
+        self.camera_stream = None
         
     class SendPhotoBehaviour(behaviour.OneShotBehaviour):
-        def __init__(self, requester_jid):
+        def __init__(self):
             super().__init__()
-            self.requester_jid = requester_jid
 
         async def run(self):
             print("Capturing image...")
-            camera = cv2.VideoCapture(0)
 
+            # Initialize the camera
+            if self.agent.camera_stream is None:
+                self.agent.camera_stream = cv2.VideoCapture(0)
+
+            camera = self.agent.camera_stream
             await asyncio.sleep(2)
 
             ret, frame = camera.read()
@@ -30,27 +38,51 @@ class CameraAgent(agent.Agent):
             filename = "photo.jpg"
             cv2.imwrite(filename, frame)
 
+            print("Image captured and saved as 'photo.jpg'.")
+
             async with aiofiles.open(filename, "rb") as img_file:
                 img_data = await img_file.read()
                 encoded_img = base64.b64encode(img_data).decode("utf-8")
 
-            msg = Message(to=self.requester_jid)
-            msg.set_metadata("performative", "inform")
-            msg.body = encoded_img
+            for agent in self.agent.registered_agents:
+                msg = Message(to=agent)
+                msg.set_metadata("performative", "inform")
+                msg.body = f"image {encoded_img}"
 
-            await self.send(msg)
-            print("Photo sent.")
-            
-    class WaitForRequestBehaviour(behaviour.CyclicBehaviour):
+                await self.send(msg)
+                print("Photo sent to ", agent)
+
+    class ListenToRegisterMessageBehaviour(behaviour.CyclicBehaviour):
         async def run(self):
             print("Waiting for request...")
             msg = await self.receive(timeout=9999)
             if msg:
-                print("Received camera image request.")
-                requester_jid = str(msg.sender)
-                self.agent.add_behaviour(self.agent.SendPhotoBehaviour(requester_jid))
+                command = msg.body.strip().lower()
+                thread = msg.thread
+                sender = str(msg.sender)
+
+                if command == "register":
+                    print("Received register request.")
+                    if sender not in self.agent.registered_agents:
+                        self.agent.registered_agents.append(sender)
+                        print(f"Registered agent: {sender}")
+                elif command == "unregister":
+                    print("Received unregister request.")
+                    if sender in self.agent.registered_agents:
+                        self.agent.registered_agents.remove(sender)
+                        print(f"Unregistered agent: {sender}")
+                else:
+                    print("Received unknown command.")
+
+            
+    class PeriodicalSendImageBehaviour(behaviour.CyclicBehaviour):
+        async def run(self):
+            await asyncio.sleep(SEND_PHOTO_INTERVAL)
+            send_photo_behaviour = self.agent.SendPhotoBehaviour()
+            self.agent.add_behaviour(send_photo_behaviour)
 
     async def setup(self):
         print(f"{self.jid} is ready.")
         # Instead of immediately sending a photo, wait for requests
-        self.add_behaviour(self.WaitForRequestBehaviour())
+        self.add_behaviour(self.ListenToRegisterMessageBehaviour())
+        self.add_behaviour(self.PeriodicalSendImageBehaviour())
